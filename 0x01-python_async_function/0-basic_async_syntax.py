9e380ce3b4cc7c7cc4db18944ce77b232075baeb
Baribor Saturday <saturdaybaribor@gmail.com>
2024-03-18 18:28:14 +0100

    Fixed requirements

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -6,7 +6,7 @@ import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    ''' waits for a random delay between 0 and max_delay 
+    ''' waits for a random delay between 0 and max_delay
     (included and float value) seconds and eventually returns it
     '''
     wait_time = random.random() * max_delay
--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -9,7 +9,7 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    '''Take the code from wait_n and alter it into a 
+    '''Take the code from wait_n and alter it into a
     new function task_wait_n
     '''
     wait_times = await asyncio.gather(
